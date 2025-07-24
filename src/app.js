038a3e1bd217d628430cf3a401c0172ac7a3462b
Marinafdez-o <mfz.oria@gmail.com>
2025-07-24 09:31:26 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
 
+let quien = ['Mi perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
+let accion = ['se comió', 'orinó sobre', 'aplastó', 'rompió'];
+let que = ['mi tarea', 'mi teléfono', 'el coche'];
+let cuando = ['antes de clase', 'mientras dormía', 'mientras hacía ejercicio', 'durante el almuerzo', 'mientras rezaba'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generarExcusa() {
+  let randomQuien = quien[Math.floor(Math.random() * quien.length)];
+  let randomAccion = accion[Math.floor(Math.random() * accion.length)];
+  let randomQue = que[Math.floor(Math.random() * que.length)];
+  let randomCuando = cuando[Math.floor(Math.random() * cuando.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let excusa = randomQuien + " " + randomAccion + " " + randomQue + " " + randomCuando + ".";
+  document.getElementById("excuse").innerHTML = excusa;
+}
+
+window.onload = generarExcusa;
